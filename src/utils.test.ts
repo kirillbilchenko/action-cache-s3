6043be9a6871a4560ca7679777b5d9937fb15320
kirillbilchenko <kirillbilchenko@users.noreply.github.com>
2023-03-22 14:01:27 +0100

    Uncomment few steps

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -4,19 +4,19 @@ import { findObject } from "./utils";
 
 describe("utils", () => {
   test("getLatestObj", async () => {
-    // const mc = new minio.Client({
-    //   endPoint: "play.min.io",
-    //   accessKey: "Q3AM3UQ867SPQQA43P2F",
-    //   secretKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
-    // });
-    // const got = await findObject(
-    //   mc,
-    //   "actions-cache",
-    //   "foo.bar",
-    //   ["test-Linux-"],
-    //   await getCompressionMethod()
-    // );
-    // expect(got).toBeTruthy();
-    // console.log(got);
+    const mc = new minio.Client({
+      endPoint: "play.min.io",
+      accessKey: "Q3AM3UQ867SPQQA43P2F",
+      secretKey: "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+    });
+    const got = await findObject(
+      mc,
+      "actions-cache",
+      "foo.bar",
+      ["test-Linux-"],
+      await getCompressionMethod()
+    );
+    expect(got).toBeTruthy();
+    console.log(got);
   });
 });
